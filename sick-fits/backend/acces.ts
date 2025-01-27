5887250a16d1de1081c196d2393d73568658702e
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2025-01-27 14:00:27 +0100

    Permission access functions

--- sick-fits/backend/acces.ts
+++ sick-fits/backend/acces.ts
@@ -1,7 +1,25 @@
 // At it's simplest, the access control is a function that returns a boolean.
 
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }): ListAccessArgs {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }): ListAccessArgs {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permissions check if somone meets a criteria - yes or no.
+
+export const permissions = {
+  ...generatedPermissions,
+};
+
+// Rule based function
